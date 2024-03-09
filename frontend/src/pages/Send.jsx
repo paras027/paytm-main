6b05e80f447fc49eaaccbe40b1353cb78fd1717c
paras027 <89771891+paras027@users.noreply.github.com>
2024-03-09 15:15:13 +0530

    added check if amount to be transfered is below the wallet money or not

--- frontend/src/pages/Send.jsx
+++ frontend/src/pages/Send.jsx
@@ -6,6 +6,7 @@ function Send() {
     
 
     const [searchParams] = useSearchParams();
+    const mm = searchParams.get("val");
     const id = searchParams.get("id");
     const name = searchParams.get("name");
     const [amount, setAmount] = useState(0);
@@ -46,6 +47,7 @@ function Send() {
                     </div>
                     <button onClick={() => {
                         try{
+                            if(val>=amount){
                         axios.put("https://payingup.onrender.com/sendmoney", {
                             to: id,
                             money:parseInt(amount)
@@ -53,8 +55,9 @@ function Send() {
                     }})
                     alert(`${amount} Sent to : ${name}`);
                 }
+                }
                     catch(e) {
-                        console.log("cant send money");
+                        console.log("Cant Send Money/Not Enough Money");
                     }
                     }} class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-teal-200 text-white">
                         Initiate Transfer
